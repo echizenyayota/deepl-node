d302ec98cb77305415c09b336a14ea2583535c02
echizenyayota <virtualmisso30@gmail.com>
2023-10-12 16:39:22 +0900

    translate text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import * as deepl from 'deepl-node';
 
 const App = () => {
+  const authKey = ""; // Replace with your key
+  const translator = new deepl.Translator(authKey);
+
+  (async () => {
+      const result = await translator.translateText('Hello', null, 'ja');
+      console.log(result.text); // こんにちは
+  })();
+
   return (
     <div className="app">
 
